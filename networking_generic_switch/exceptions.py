4ca43a1ee918a979b087f7fc1ac55f83d86faf00
Vasyl Saienko <vsaienko@mirantis.com>
2016-12-09 14:54:15 +0200

    Add vlan aware VMs support
    With this patch ngs starts supporting attaching of trunk port to
    baremetal server. Only VLAN Neutron network is supported.
    
    There are two ways to configure trunk port:
    
     * C1: When segmentation details for trunk ports are inherited from
       Neutron network. VLAN translation support is not required.
       Added implementation for cisco, arista, OVS on Linux.
    
     * C2: When user set segmentation details for trunk port explicitly.
       Switch should support VLAN translation for this case.
       Implement only for OVS on Linux, experimental.
    
    NetmikoSwitch.plug_port_to_network() is deprecated. New bind_port()
    should be used instead.
    
    New switch config option: vlan_translation_supported was introduced.
    This option defines if switch support vlan translation which affect
    the way how trunk is configured.
    
    Tests are amended to comply with new pep8 rule H210 introduced with
    change Id9e98f26ecfc314c7f3e69fd518abfebedc39b1a.
    
    Co-Authored-By: Will Szumski <will@stackhpc.com>
    Co-Authored-By: Mark Goddard <mark@stackhpc.com>
    Co-Authored-By: Seunghun Lee <seunghun@stackhpc.com>
    
    Change-Id: If084382f4c17438e5142f51f88d6e436f8b85082
    (cherry picked from commit 1163a1f062dbf53d9eb5891effb8182fee6a0c21)
    (cherry picked from commit 7dfe3e2f16cc17f10f07ea389298aa95548a1127)
    (cherry picked from commit c54e9ba5d4a65a5b2b3ad8a7ae1a58e5ac017e1a)
    (cherry picked from commit 701e93f85487b372d6a4dd732a242ef4bb0a4d93)
    (cherry picked from commit 0196f837529fd6510ba2746425ae29e844afef09)
    (cherry picked from commit 2feadb09afc94f16e892c71be06b6ce2221112d1)

--- networking_generic_switch/exceptions.py
+++ networking_generic_switch/exceptions.py
@@ -53,3 +53,8 @@ class GenericSwitchNetmikoConfigError(GenericSwitchException):
 
 class GenericSwitchBatchError(GenericSwitchException):
     message = _("Batching error: %(device)s, error: %(error)s")
+
+
+class GenericSwitchNotSupported(GenericSwitchException):
+    message = _("Requested feature is not supported by "
+                "networking-generic-switch. %(error)s")
