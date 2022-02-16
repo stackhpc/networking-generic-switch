d6cb67e2551388e2a60a1a7640163af707068b32
Will Szumski <will@stackhpc.com>
2022-02-16 09:32:12 +0000

    fixup! add DellOS10 device type with updated commands
    - Support port channels
    - Baremetal ports created by ironic already contain the word, 'ethernet'.

--- networking_generic_switch/devices/netmiko_devices/dell.py
+++ networking_generic_switch/devices/netmiko_devices/dell.py
@@ -33,39 +33,39 @@ class DellOS10(netmiko_devices.NetmikoSwitch):
     )
 
     PLUG_PORT_TO_NETWORK = (
-        "interface ethernet {port}",
+        "interface {port}",
         "switchport mode access",
         "switchport access vlan {segmentation_id}",
         "exit",
     )
 
     DELETE_PORT = (
-        "interface ethernet {port}",
+        "interface {port}",
         "no switchport access vlan",
         "exit",
     )
 
     ADD_NETWORK_TO_TRUNK = (
-        "interface ethernet {port}",
+        "interface {port}",
         "switchport mode trunk",
         "switchport trunk allowed vlan {segmentation_id}",
         "exit",
     )
 
     REMOVE_NETWORK_FROM_TRUNK = (
-        "interface ethernet {port}",
+        "interface {port}",
         "no switchport trunk allowed vlan {segmentation_id}",
         "exit",
     )
 
     ENABLE_PORT = (
-        "interface ethernet {port}",
+        "interface {port}",
         "no shutdown",
         "exit",
     )
 
     DISABLE_PORT = (
-        "interface ethernet {port}",
+        "interface {port}",
         "shutdown",
         "exit",
     )
